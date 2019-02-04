e995f157dc45ebcda76cc43ec9da56c84f462787
Brad-Tudor-Eng <brad.tudor.eng@gmail.com>
2019-02-04 06:16:36 -0800

    merged user and device

--- web/server/resolvers/User/query.js
+++ web/server/resolvers/User/query.js
@@ -2,20 +2,31 @@ import { ObjectID } from 'mongodb'
 import User from '../../models/User'
 import jwt from 'jsonwebtoken'
 
+//find the user,
+//look at user devices
+//if devices have records strip data away from all but first
+//return user
+
+
+
 export const findUser = async (parent, {data}, ctx, info) =>{
     let token = data.token
     token = jwt.verify(token, process.env.JWT_KEY)
     const id = ObjectID(token.id)
-    const user = await User.findById(id).populate('device')
+    let user = await User.findById(id).populate('device')
     if(user){
-    /*
-        if the user has a device
-        find all records
-        keep first record,
-        return just the dates for all other records.
-    */
-
-
+//fix nested loop
+        if (user.device.length > 0){
+            let devices = user.device
+            devices.forEach((device,i)=>{
+                let records = device.records
+                records = records.map((record,i)=>{
+                    return i === 0 ? record : { date: record.date, data: [] } 
+                })
+                device.records = records
+            })
+            user.device = devices
+        }
 
         return user
     }else{
